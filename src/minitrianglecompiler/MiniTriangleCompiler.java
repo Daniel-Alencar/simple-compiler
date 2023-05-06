21561a103885fc22e800b2bd49d57a47a0ddf6d7
Daniel-Alencar <danielalencar746@gmail.com>
2023-05-06 16:42:22 -0300

    Initial commit

---
+++ src/minitrianglecompiler/MiniTriangleCompiler.java
@@ -0,0 +1,17 @@
+package minitrianglecompiler;
+
+/**
+ *
+ * @author engenheiro
+ */
+public class MiniTriangleCompiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+    
+}
