e1fb706b8eb60ba4450b179b5443a189df331456
Daniel-Alencar <danielalencar746@gmail.com>
2023-05-26 11:50:00 -0300

    Mexendo no Home

---
+++ src/minitrianglecompiler/Parser.java
@@ -0,0 +1,29 @@
+package minitrianglecompiler;
+
+public class Parser {
+  private TerminalSymbol currentTerminal;
+
+  private void accept(TerminalSymbol expectedTerminal) {
+    
+  }
+
+  private void parsePrograma();
+  private void parseId();
+  private void parseLetra();
+  private void parseCorpo();
+  private void parseDigito();
+  private void parseDeclaracaoDeVariaveis();
+  private void parseComandoComposto();
+  private void parseTipoSimples();
+  private void parseComando();
+  private void parseAtribuicao();
+  private void parseCondicional();
+  private void parseIterativo();
+  private void parseComandoComposto();
+  private void parseExpressao();
+  private void parseCondicional();
+  private void parseVazio();
+  private void parseExpressaoSimples();
+  private void parseOpRel();
+  private void parseTermo();
+}
