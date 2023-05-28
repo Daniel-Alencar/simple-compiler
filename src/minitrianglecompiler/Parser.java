218d7168ee32b80391d338d0970bb5d8d56f729a
Daniel-Alencar <danielalencar746@gmail.com>
2023-05-28 19:37:27 -0300

    Organizando

--- src/minitrianglecompiler/Parser.java
+++ src/minitrianglecompiler/Parser.java
@@ -5,23 +5,30 @@ public class Parser {
 
   private void accept(TerminalSymbol expectedTerminal) {}
 
-  private void parsePrograma();
-  private void parseId();
-  private void parseLetra();
-  private void parseCorpo();
-  private void parseDigito();
-  private void parseDeclaracaoDeVariaveis();
-  private void parseComandoComposto();
-  private void parseTipoSimples();
-  private void parseComando();
-  private void parseAtribuicao();
-  private void parseCondicional();
-  private void parseIterativo();
-  private void parseComandoComposto();
-  private void parseExpressao();
-  private void parseCondicional();
-  private void parseVazio();
-  private void parseExpressaoSimples();
-  private void parseOpRel();
-  private void parseTermo();
+  private void parse_atribuicao();
+  private void parse_comando();
+  private void parse_comandoComposto();
+  private void parse_condicional();
+  private void parse_corpo();
+  private void parse_declaracao();
+  private void parse_declaracaoDeVariavel();
+  private void parse_declaracoes();
+  private void parse_expressao();
+  private void parse_expressaoSimples();
+  private void parse_fator();
+  private void parse_iterativo();
+  private void parse_listaDeComandos();
+  private void parse_listaDeIds();
+  private void parse_outros();
+  private void parse_programa();
+  private void parse_termo();
+  private void parse_tipo();
+  private void parse_variavel();
+
+  // Regras da gramática LL1 como métodos
+  private void parse_atribuicao() {
+    parse_variavel();
+    accept(":=");
+    parse_expressao();
+  }
 }
