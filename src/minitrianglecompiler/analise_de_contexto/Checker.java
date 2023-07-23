78b864f55e3259daa6567e848f950ea88c7d7f6d
Richard <richard.esclima@gmail.com>
2023-07-23 17:59:50 -0300

    implementação de metodos do checker

--- src/minitrianglecompiler/analise_de_contexto/Checker.java
+++ src/minitrianglecompiler/analise_de_contexto/Checker.java
@@ -31,140 +31,208 @@ public class Checker implements Visitor {
 
     @Override
     public void visit_nodeComando(nodeComando comando) {
-        identificationTable.openScope();
-        comando.visit(this);
+        if (comando != null)
+            comando.visit(this);
     }
 
     @Override
     public void visit_nodeComandoAtribuicao(nodeComandoAtribuicao comando) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeComandoAtribuicao'");
+        if (comando != null)
+            identificationTable.enter(comando.variavel, null);
+        comando.visit(this);
     }
 
     @Override
     public void visit_nodeComandoComposto(nodeComandoComposto comando) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeComandoComposto'");
+        if (comando != null) {
+            comando.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeComandoCondicional(nodeComandoCondicional comando) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeComandoCondicional'");
+        if (comando != null) {
+            if (comando.expressao != null) {
+                // verificar se a expressão é booleana
+                if (comando.comando1 != null) {
+                    comando.comando1.visit(this);
+                }
+                if (comando.comando2 != null) {
+                    comando.comando2.visit(this);
+                }
+            }
+        }
+        // erro semantico caso contrário
     }
 
     @Override
     public void visit_nodeComandoIterativo(nodeComandoIterativo comando) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeComandoIterativo'");
+        if (comando != null) {
+            // verificar se a expressão é booleana
+            if (comando.expressao != null) {
+                if (comando.comando != null) {
+                    comando.comando.visit(this);
+                }
+            }
+        }
     }
 
     @Override
     public void visit_nodeCorpo(nodeCorpo corpo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeCorpo'");
+        if (corpo != null) {
+            corpo.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeDeclaracao(nodeDeclaracao declaracao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeDeclaracao'");
+        if (declaracao != null) {
+            declaracao.declaracaoDeVariavel.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeDeclaracaoDeVariavel(nodeDeclaracaoDeVariavel declaracao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeDeclaracaoDeVariavel'");
+        // checar se o tipo da declaração é valido
+        identificationTable.openScope();
+        for (int i = 0; i < declaracao.IDs.size(); i++) {
+            if (identificationTable.retrieve(declaracao.IDs.get(i).valor) == null) {
+                identificationTable.enter(declaracao.IDs.get(i).valor, null);
+            } else {
+                // erro variavel já declarada
+            }
+        }
+        declaracao.visit(this);
+        identificationTable.closeScope();
     }
 
     @Override
     public void visit_nodeDeclaracoes(nodeDeclaracoes declaracoes) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeDeclaracoes'");
+        if (declaracoes != null) {
+            for (int i = 0; i < declaracoes.declaracoes.size(); i++) {
+                declaracoes.declaracoes.get(i).visit(this);
+            }
+        }
     }
 
     @Override
     public void visit_nodeExpressao(nodeExpressao expressao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeExpressao'");
+        if (expressao != null) {
+            if (expressao.expressaoSimples1 != null) {
+                expressao.expressaoSimples1.visit(this);
+            }
+            if (expressao.expressaoSimples2 != null) {
+                expressao.expressaoSimples1.visit(this);
+            }
+            if (expressao.operadorRelacional != null) {
+                expressao.operadorRelacional.visit(this);
+            }
+        }
     }
 
     @Override
     public void visit_nodeExpressaoSimples(nodeExpressaoSimples expressao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeExpressaoSimples'");
+        if (expressao != null) {
+            if (expressao.termo != null) {
+                expressao.termo.visit(this);
+            }
+            if (expressao.operadoresAditivos != null) {
+                for (int i = 0; i < expressao.operadoresAditivos.size(); i++) {
+                    if (expressao.operadoresAditivos.get(i) != null) {
+                        expressao.operadoresAditivos.get(i).visit(this);
+                        expressao.termos.get(i).visit(this);
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public void visit_nodeFator(nodeFator fator) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeFator'");
+        if (fator instanceof nodeVariavel) {
+            ((nodeVariavel) fator).visit(this);
+        } else if (fator instanceof nodeLiteral) {
+            ((nodeLiteral) fator).visit(this);
+        } else if (fator instanceof nodeExpressao) {
+            ((nodeExpressao) fator).visit(this);
+        }
     }
 
     @Override
     public void visit_nodeID(nodeID ID) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeID'");
+        if (ID != null) {
+            ID.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeLiteral(nodeLiteral literal) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeLiteral'");
+        if (literal != null) {
+            literal.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeOperador(nodeOperador operador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeOperador'");
+        if (operador != null) {
+            operador.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeOperadorAditivo(nodeOperadorAditivo operador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeOperadorAditivo'");
+        if (operador != null) {
+            operador.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeOperadorMultiplicativo(nodeOperadorMultiplicativo operador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeOperadorMultiplicativo'");
+        if (operador != null) {
+            operador.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeOperadorRelacional(nodeOperadorRelacional operador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeOperadorRelacional'");
+        if (operador != null) {
+            operador.visit(this);
+        }
     }
 
     @Override
     public void visit_nodePrograma(nodePrograma programa) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodePrograma'");
+        if (programa != null) {
+            programa.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeTermo(nodeTermo termo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeTermo'");
+        if (termo != null) {
+            termo.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeTipo(nodeTipo tipo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeTipo'");
+        if (tipo != null) {
+            tipo.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeTipoSimples(nodeTipoSimples tipoSimples) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeTipoSimples'");
+        if (tipoSimples != null) {
+            tipoSimples.visit(this);
+        }
     }
 
     @Override
     public void visit_nodeVariavel(nodeVariavel variavel) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit_nodeVariavel'");
+        if (variavel != null && identificationTable.retrieve(variavel.ID.valor) != null) {
+            variavel.visit(this);
+        }
     }
 
 }
