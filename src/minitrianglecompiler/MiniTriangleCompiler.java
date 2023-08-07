53a48c5ed6dd8e4ac6b102b220fa1a673e7a30e4
Daniel-Alencar <danielalencar746@gmail.com>
2023-08-07 02:08:02 -0300

    Especificando modo de compilar

--- src/minitrianglecompiler/MiniTriangleCompiler.java
+++ src/minitrianglecompiler/MiniTriangleCompiler.java
@@ -14,51 +14,67 @@ import minitrianglecompiler.visitor.*;
  */
 public class MiniTriangleCompiler {
 
-	public static int codeNumber = 0;
+	public static String fileName = "/test/code0.txt";
+	public static int steps = 5;
 
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner("/test/code" + codeNumber + ".txt");
+		Scanner scanner = new Scanner(fileName);
 		int counter = 0;
 		ArrayList<Token> arrayOfTokens = new ArrayList<>();
+		nodePrograma programaAST = null;
 
-		try {
-			do {
-				Token currentToken = scanner.scan();
-				arrayOfTokens.add(counter, currentToken);
+		if(steps >= 1) {
+			// Análise léxica
+			try {
+				do {
+					Token currentToken = scanner.scan();
+					arrayOfTokens.add(counter, currentToken);
 
-				counter++;
-			} while (scanner.isEOF() == false);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+					counter++;
+				} while (scanner.isEOF() == false);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 
-		for(int i = 0; i < arrayOfTokens.size(); i++ ) {
-			if(arrayOfTokens.get(i).kind == Token.ERROR) {
-				Token token = arrayOfTokens.get(i);
-				new ShowError(
-					"Token \"" + token.spelling + "\" inválido\n" + 
-					"Linha: " + token.line + " Coluna: " + token.column
-				);
+			for(int i = 0; i < arrayOfTokens.size(); i++ ) {
+				if(arrayOfTokens.get(i).kind == Token.ERROR) {
+					Token token = arrayOfTokens.get(i);
+					new ShowError(
+						"Token \"" + token.spelling + "\" inválido" + "\n" + 
+						"Linha: " + token.line + " Coluna: " + token.column
+					);
+				}
 			}
 		}
 
-		Parser sintaticParser = new Parser(arrayOfTokens);
-		nodePrograma programaAST = sintaticParser.parse();
+		if(steps >= 2) {
+			// Análise Sintática
+			Parser sintaticParser = new Parser(arrayOfTokens);
+			programaAST = sintaticParser.parse();
+		}
 
-		System.out.println("");
-		Printer printer = new Printer();
-		printer.print(programaAST);
+		if(steps >= 3) {
+			// Printagem da AST (Árvore sintática abstrata)	
+			System.out.println("");
+			Printer printer = new Printer();
+			printer.print(programaAST);
+		}
 
-		Checker checker = new Checker();
-		checker.check(programaAST);
+		if(steps >= 4) {
+			// Análise de contexto
+			Checker checker = new Checker();
+			checker.check(programaAST);
+		}
 
-		CodeGenerator codeGenerator = new CodeGenerator();
-
-		codeGenerator.openFile("Object-code" + codeNumber + ".txt");
-		codeGenerator.printCode(programaAST);
-		codeGenerator.closeFile();
+		if(steps >= 5) {
+			// Geração de código
+			CodeGenerator codeGenerator = new CodeGenerator();
+			codeGenerator.openFile("Object-code.txt");
+			codeGenerator.printCode(programaAST);
+			codeGenerator.closeFile();
+		}
 	}
 }
